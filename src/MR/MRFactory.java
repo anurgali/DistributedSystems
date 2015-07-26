0fb1d831429b6c35c088bf9664d5ce84b08516d5
anurgali <anurgali@andrew.cmu.edu>
2015-07-26 09:36:02 +0300

    Dummy MR created

--- src/MR/MRFactory.java
+++ src/MR/MRFactory.java
@@ -3,16 +3,16 @@ package MR;
 public class MRFactory {
 
 	public static IMapper getMapper(String name){
-		if (name.equals("WORD COUNT")){
+		if (name.equals("WORD_COUNT")){
 			return new WordCountMapper();
 		}
-		else return null;
+		else return new DummyMapper();
 	}
 	
 	public static IReducer getReducer(String name){
-		if (name.equals("WORD COUNT")){
+		if (name.equals("WORD_COUNT")){
 			return new WordCountReducer();
 		}
-		else return null;
+		else return new DummyReducer();
 	}
 }
